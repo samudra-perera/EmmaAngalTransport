bd63fa8d41e167830ff1cce6e38a8fdc6b5210e6
Samudra Perera <samudrapup@gmail.com>
2023-05-11 20:50:59 -0400

    Completed the mobile styling for the clients component:

--- emma-angal-transport/src/components/Testimonials/Style/Testimonials.style.js
+++ emma-angal-transport/src/components/Testimonials/Style/Testimonials.style.js
@@ -1,3 +1,4 @@
+import { media } from "@/styles/media";
 import styled from "styled-components";
 
 const TestimonialContainer = styled.div`
@@ -10,17 +11,38 @@ const TestimonialCardContainer = styled.div`
   background-color: #7eb4fb;
   padding: 3rem 5rem;
   margin-top: 2rem;
+
+  @media ${media.tablet} {
+    padding: 3rem;
+  }
+
 `;
 
 const QuoteImage = styled.img`
   height: 65px;
   width: 65px;
+
+  @media ${media.mobile} {
+    height: 40px;
+    width: 40px;
+  }
+
 `;
 
 const TestimonialText = styled.p`
   width: 90%;
   padding-top: 2rem;
   padding-bottom: 3rem;
+
+  @media ${media.tablet} {
+    font-size: .9rem;
+  }
+
+  @media ${media.mobile} {
+    font-size: .8rem;
+    width: 100%;
+  }
+
 `;
 
 const AvatarContainer = styled.div`
@@ -28,12 +50,32 @@ const AvatarContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    @media ${media.tablet} {
+        padding-left: 1rem;
+    }
+
+    @media ${media.mobile} {
+        padding-left: 0;
+    }
 `;
 
 const Avatar = styled.img`
     height: 80px;
     width: 80px;
     margin-right: 1.5rem;
+
+@media ${media.tablet} {
+    height: 60px;
+    width: 60px;
+    margin-right: 1rem;
+}
+
+@media ${media.mobile} {
+    height: 50px;
+    width: 50px;
+}
+
 `;
 const AvatarName = styled.span`
     font-size: .8rem;
